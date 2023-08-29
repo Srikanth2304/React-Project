8bad60868a107c5c4345beca3322d10424c632a7
Srikanth2304 <59932476+Srikanth2304@users.noreply.github.com>
2023-08-29 15:51:41 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,14 +1,13 @@
-
 import React, { useState } from 'react';
 import './style/Main.css';
 // font awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link,NavLink } from 'react-router-dom/cjs/react-router-dom.min'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Navbar() {
     return <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg " style={{background: "linear-gradient(to right, #0099CC, #006699)"}}>
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Standard_Chartered_%282021%29.svg/2560px-Standard_Chartered_%282021%29.svg.png"
@@ -65,18 +64,258 @@ function Navbar() {
 
 export default Navbar;
 
-.navbar.bg-body-tertiary {
-    background: linear-gradient(to right, #007BFF, #00C6FF);
+
+
+*{
+    font-family: 'Poppins', sans-serif;
+}
+/* home page style */
+footer {
+    color: rgb(255, 255, 255);
 }
 
+a {
+    color: white;
+}
 
-<link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-/>
+.shfooter .collapse {
+    display: inherit;
+}
 
-      <nav class="navbar navbar-expand-lg" style={{background: "linear-gradient(to right, #003366, #66ccff)"}}>
+@media (max-width: 767px) {
+    .shfooter ul {
+        margin-bottom: 0;
+    }
 
-.light-blue-gradient {
-    background: linear-gradient(to right, rgb(51, 153, 255), rgb(0, 102, 204));
+    .shfooter .collapse {
+        display: none;
+    }
+
+    .shfooter .collapse.show {
+        display: block;
+    }
+
+    .shfooter .title .fa-angle-up,
+    .shfooter .title[aria-expanded="true"] .fa-angle-down {
+        display: none;
+    }
+
+    .shfooter .title[aria-expanded="true"] .fa-angle-up {
+        display: block;
+    }
+
+    .shfooter .navbar-toggler {
+        display: inline-block;
+        padding: 0;
+    }
+}
+
+.resize {
+    text-align: center;
+}
+
+.resize {
+    margin-top: 3rem;
+    font-size: 1.25rem;
+}
+
+/*RESIZESCREEN ANIMATION*/
+.fa-angle-double-right {
+    animation: rightanime 1s linear infinite;
+}
+
+.fa-angle-double-left {
+    animation: leftanime 1s linear infinite;
+}
+
+@keyframes rightanime {
+    50% {
+        transform: translateX(10px);
+        opacity: 0.5;
+    }
+
+    100% {
+        transform: translateX(10px);
+        opacity: 0;
+    }
+}
+
+@keyframes leftanime {
+    50% {
+        transform: translateX(-10px);
+        opacity: 0.5;
+    }
+
+    100% {
+        transform: translateX(-10px);
+        opacity: 0;
+    }
+}
+
+/*Login area style*/
+@import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+
+html,
+.login_wrapper {
+    height: 100%;
+}
+
+.login_wrapper {
+    display: grid;
+    place-items: center;
+    background: #dde1e7;
+    background-image: url("loginback2.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100%;
+    width: 100%;
+    text-align: center;
+}
+.login_content {
+    width: 330px;
+    padding: 30px 25px;
+    background: #f8f9fa;
+    border-radius: 10px;
+    box-shadow: -3px -3px 7px #ffffff73,
+        2px 2px 5px rgba(94, 104, 121, 0.288);
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    margin: 40px 0;
+}
+
+.login_content .text {
+    font-size: 30px;
+    font-weight: 600;
+    margin-bottom: 15px;
+    color: #595959;
+}
+
+.field {
+    height: 40px;
+    width: 100%;
+    display: flex;
+    position: relative;
+}
+.field:nth-child(2) {
+    margin-top: 15px;
+}
+.field:nth-child(3) {
+    margin-top: 15px;
+}
+
+.field input {
+    height: 100%;
+    width: 100%;
+    padding-left: 45px;
+    outline: none;
+    border: none;
+    font-size: 16px;
+    background: #dde1e7;
+    color: #595959;
+    border-radius: 25px;
+    box-shadow: inset 2px 2px 5px #BABECC,
+        inset -5px -5px 10px #ffffff73;
+}
+.field input:hover {
+    background: #ced0d2;
+}
+.field input:focus {
+    box-shadow: inset 1px 1px 2px #BABECC,
+        inset -1px -1px 2px #ffffff73;
+}
+
+.field select {
+    height: 100%;
+    width: 100%;
+    padding-left: 45px;
+    outline: none;
+    border: none;
+    font-size: 16px;
+    background: #dde1e7;
+    color: #595959;
+    border-radius: 25px;
+    box-shadow: inset 2px 2px 5px #BABECC,
+        inset -5px -5px 10px #ffffff73;
+}
+.field select:hover {
+    background: #ced0d2;
+}
+.field select:focus {
+    background: #dde1e7;
+    color: #595959;
+    box-shadow: inset 1px 1px 2px #BABECC,
+        inset -1px -1px 2px #ffffff73;
+}
+
+.field span {
+    position: absolute;
+    color: #595959;
+    width: 50px;
+    line-height: 40px;
+}
+
+.field label {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 45px;
+    pointer-events: none;
+    color: #666666;
+}
+
+.field input:valid~label {
+    opacity: 0;
+}
+
+.forgot-pass {
+    text-align: left;
+    margin: 6px 0 10px 5px;
+}
+
+.forgot-pass a {
+    font-size: 16px;
+    color: #3498db;
+    text-decoration: none;
+}
+
+.forgot-pass:hover a {
+    text-decoration: underline;
+}
+
+.signin_button {
+    margin: 2px 0;
+    width: 100%;
+    height: 45px;
+    font-size: 18px;
+    line-height: 45px;
+    font-weight: 600;
+    background: #35d300;
+    border-radius: 25px;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    color: #595959;
+    box-shadow: 2px 2px 5px #BABECC,
+        -5px -5px 10px #ffffff73;
+}
+
+.signin_button:focus {
+    color: #3498db;
+    box-shadow: inset 2px 2px 5px #BABECC,
+        inset -5px -5px 10px #ffffff73;
+}
+
+.sign-up {
+    margin: 10px 0;
+    color: #595959;
+    font-size: 16px;
+}
+
+.sign-up a {
+    color: #3498db;
+    text-decoration: none;
+}
+
+.sign-up a:hover {
+    text-decoration: underline;
 }
